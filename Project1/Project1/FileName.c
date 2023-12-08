e079d9cc2028b1dc1c628ad21cb423a507dc68bd
KimSH <36595569+KimSH1238@users.noreply.github.com>
2023-12-08 21:42:33 +0900

    Add files via upload

---
+++ Project1/Project1/FileName.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("first print\n");
+	printf("second print");
+	return 0;
+}
---
+++ Project1/Project1/calculate.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int calcul(void)
+{
+	int a = 3;
+	int b = 2;
+	printf("결과값=%d\n", a + b);
+	printf("결과값=%d\n", a - b);
+	printf("결과값=%d\n", a * b);
+	printf("결과값=%d\n", a / b);
+	
+	return 0;
+}
